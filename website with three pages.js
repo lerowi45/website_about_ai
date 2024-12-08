d47c802e81cca3b1d240e93e30930dae5db6aa1e
lerowi45 <lewekies@gmail.com>
2024-12-08 17:19:06 +0000

    initial commit

---
+++ website with three pages.js
@@ -0,0 +1,10 @@
+/*function showMenu (s) {
+	s.classList.toggle("hideit");
+} */
+
+function changeMenuBars (x) {
+	x.classList.toggle("change");
+	document.getElementById("dropDown").classList.toggle("show");
+	//hideMenu.target.classList.toggle("hideit");
+	//document.getElementById("dropDown").click();
+}
